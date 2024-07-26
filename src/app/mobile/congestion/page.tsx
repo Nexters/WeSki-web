aa1581dde736ce37541d019cd5dc844d20c4bc15
Najeong-Kim <atpeoe1023@gmail.com>
2024-07-26 22:45:45 +0900

    feat: Update congestion page

--- src/app/mobile/congestion/page.tsx
+++ src/app/mobile/congestion/page.tsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+
+import { cn } from '@/lib/utils';
+
 const Page = () => {
-  return <p>혼잡도</p>;
+  return (
+    <div className={cn('h-[calc(100%-154px)] overflow-scroll scrollbar-hide')}>
+      {Array.from({ length: 4 }).map((_, index) => (
+        <Image
+          key={index}
+          src={`/congestion/mock${(index % 3) + 1}.png`}
+          alt={`${index}`}
+          width={376}
+          height={461}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Page;
