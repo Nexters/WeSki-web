cdc316ee59723677b851998af83ef4a53ad55dc1
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-07-26 16:53:36 +0900

    feat: 날씨 페이지 퍼블리싱

--- src/app/mobile/weather/page.tsx
+++ src/app/mobile/weather/page.tsx
@@ -1,5 +1,78 @@
+import CloudIcon from "@/components/icons/cloud";
+import SunIcon from "@/components/icons/sun";
+import { cn } from "@/lib/utils";
+
+const WEATHER_DATA = [
+  {
+    time: "오전 8시",
+    icon: <SunIcon />,
+    temperature: "22°",
+    humidity: "20%",
+  },
+  {
+    time: "오전 11시",
+    icon: <CloudIcon />,
+    temperature: "24°",
+    humidity: "30%",
+  },
+  {
+    time: "오후 2시",
+    icon: <SunIcon />,
+    temperature: "26°",
+    humidity: "40%",
+  },
+  {
+    time: "오후 5시",
+    icon: <CloudIcon />,
+    temperature: "25°",
+    humidity: "30%",
+  },
+];
+
 const Page = () => {
-  return <p>날씨</p>;
+  return (
+    <div
+      className={cn("flex flex-col gap-3 items-center overflow-auto h-[650px]")}
+    >
+      {Array.from({ length: 3 }).map((_, index) => (
+        <WeatherCard key={index} />
+      ))}
+    </div>
+  );
 };
 
 export default Page;
+
+const WeatherCard = () => {
+  return (
+    <div className={cn("rounded-lg w-[335px] h-[461px] bg-gray-100 p-6 ")}>
+      <div className={cn("flex justify-between items-center mb-[130px]")}>
+        <h4 className={cn(" font-bold")}>하이원 스키장</h4>
+        <div className={cn("text-sm font-bold rounded-lg bg-white px-3 py-1")}>
+          설질 GOOD 56%
+        </div>
+      </div>
+
+      <p className={cn(" text-4xl font-semibold")}>25°</p>
+      <p className={cn(" text-sm text-gray-600 mb-4")}>흐리고 비</p>
+
+      <p className={cn("text-lg font-semibold")}>
+        흐리고 비오는 날씨가 계속 돼요
+      </p>
+      <p className={cn("text-sm mb-4")}>최고:28° 최저 24° 미세먼지 좋음</p>
+
+      <hr className={cn("mb-4")} />
+
+      <ul className={cn("flex gap-5")}>
+        {WEATHER_DATA.map((data) => (
+          <li key={data.time} className={cn("flex items-center flex-col")}>
+            <div className={cn(" text-sm font-medium mb-1")}>{data.time}</div>
+            {data.icon}
+            <p className={cn("mt-1")}>{data.temperature}</p>
+            <p className={cn("text-xs")}>{data.humidity}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ src/components/icons/cloud.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CloudIcon = () => {
+  return (
+    <svg
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.9001 16.5C16.2142 16.5 18.9001 13.8135 18.9001 10.5C18.9001 7.18652 16.2142 4.5 12.9001 4.5C10.3899 4.5 8.23838 6.04219 7.34307 8.23066C7.01025 8.14512 6.66104 8.1 6.3001 8.1C3.97979 8.1 2.1001 9.98027 2.1001 12.3C2.1001 14.6197 3.97979 16.5 6.3001 16.5H12.9001Z"
+        stroke="#191F28"
+        stroke-width="1.5"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CloudIcon;
