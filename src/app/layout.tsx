1ff23c5f6b391a1dbb449d5d5ebc6f0589aa18af
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-06 00:31:35 +0900

    chore: tanstack query 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/app/globals.css';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Providers from './_providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
