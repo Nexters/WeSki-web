03807b588e9dae787f182c2c42a41012e4f0b498
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-12 16:03:27 +0900

    feat: camerabutton 외부에서 변경 가능하도록 변경

--- src/shared/ui/cam-button.tsx
+++ src/shared/ui/cam-button.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import CameraIcon from '../icons/camera';
 import { cn } from '../lib';
 
-const CameraButton = () => {
+type CameraButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const CameraButton: React.FC<CameraButtonProps> = ({ className, ...args }) => {
   return (
     <button
-      className={cn('flex h-[24px] w-[24px] items-center justify-center rounded-[7px] bg-gray-90')}
+      className={cn(
+        'flex h-[24px] w-[24px] items-center justify-center rounded-[7px] bg-gray-90',
+        className
+      )}
+      {...args}
     >
       <CameraIcon />
     </button>
