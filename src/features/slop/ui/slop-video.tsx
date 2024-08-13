50ea940f434608cfb7fb437c24f12018f16cbbd2
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-13 19:00:56 +0900

    feat: 비디오 열릴시 다른 요소와의 상호작용 막음

--- src/features/slop/ui/slop-video.tsx
+++ src/features/slop/ui/slop-video.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactHlsPlayer from 'react-hls-player';
 import { cn } from '@/shared/lib';
 import CloseButton from '@/shared/ui/close-button';
@@ -11,6 +11,22 @@ interface SlopVideoProps {
 const SlopVideo = ({ src, closeVideo }: SlopVideoProps) => {
   const playerRef = React.useRef<HTMLVideoElement>(null);
 
+  useEffect(() => {
+    const player = playerRef.current;
+
+    function fireOnVideoStart() {
+      document.body.classList.add('video-active');
+      player?.focus();
+    }
+
+    player?.addEventListener('play', fireOnVideoStart);
+
+    return () => {
+      player?.removeEventListener('play', fireOnVideoStart);
+      document.body.classList.remove('video-active');
+    };
+  }, [playerRef]);
+
   return (
     <>
       <ReactHlsPlayer
@@ -18,9 +34,9 @@ const SlopVideo = ({ src, closeVideo }: SlopVideoProps) => {
         playerRef={playerRef}
         src={src}
         autoPlay={true}
-        controls={true}
-      ></ReactHlsPlayer>
-      <CloseButton className="absolute right-4 top-4 z-[33]" onClick={closeVideo} />
+        controls={false}
+      />
+      <CloseButton className="video-close absolute right-4 top-4 z-[33]" onClick={closeVideo} />
     </>
   );
 };
