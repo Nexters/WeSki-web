eddc9f7d21c9a2fb2982974b0937cae21f371422
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-13 17:08:58 +0900

    refactor: map 분리

---
+++ src/features/slop/ui/slop-map.tsx
@@ -0,0 +1,54 @@
+import { animated } from '@react-spring/web';
+import type { StaticImageData } from 'next/image';
+import Image from 'next/image';
+import type { ComponentType } from 'react';
+import React from 'react';
+import type { Level } from '@/entities/slop/model/model';
+import { cn } from '@/shared/lib';
+import useMapPinch from '../hooks/useMapPinch';
+
+interface SlopMapProps {
+  containerRef: React.RefObject<HTMLElement>;
+  children: React.ReactNode;
+  mapSrc: StaticImageData;
+
+  slops: {
+    id: string;
+    level: Level;
+    Element: ComponentType<{
+      color?: string;
+    }>;
+  }[];
+  selectedSlop: string | null;
+}
+
+const SlopMap = ({ containerRef, children, mapSrc, slops, selectedSlop }: SlopMapProps) => {
+  const { ref, style } = useMapPinch(containerRef);
+
+  return (
+    <animated.div
+      ref={ref}
+      style={{
+        touchAction: 'none',
+        display: 'inline-block',
+        width: '100%',
+        height: '100%',
+        ...style,
+      }}
+    >
+      <Image src={mapSrc} alt="이미지" width={420} height={750} />
+
+      {slops.map((slop) => (
+        <div key={slop.id} className={cn('absolute top-0 w-full')}>
+          <slop.Element
+            color={selectedSlop !== slop.id && selectedSlop !== null ? 'fill-gray-40' : undefined}
+          />
+        </div>
+      ))}
+
+      {children}
+    </animated.div>
+  );
+};
+
+export default SlopMap;
--- src/widgets/webcam/ui/webcam-map.tsx
+++ src/widgets/webcam/ui/webcam-map.tsx
@@ -1,14 +1,10 @@
-import { animated } from '@react-spring/web';
 import type { StaticImageData } from 'next/image';
-import Image from 'next/image';
 import type { ComponentType } from 'react';
 import React, { useRef } from 'react';
+import SlopMap from '@/features/slop/ui/slop-map';
+import SlopTooltip from '@/features/slop/ui/slop-tooltip';
 import type { Level } from '@/entities/slop/model/model';
 import { cn } from '@/shared/lib';
-import CameraButton from '@/shared/ui/cam-button';
-
-import { Tooltip } from '@/shared/ui/tooltip';
-import useMapPinch from '../hooks/useMapPinch';
 
 interface WebcamMapProps {
   slops: {
@@ -32,48 +28,30 @@ interface WebcamMapProps {
 
 const WebcamMap = ({ slops, mapSrc, selectedSlop }: WebcamMapProps) => {
   const containerRef = useRef<HTMLElement>(null);
-  const { ref, style } = useMapPinch(containerRef);
 
   return (
     <section className={cn('relative aspect-[25/14] w-full overflow-hidden')} ref={containerRef}>
-      <animated.div
-        ref={ref}
-        style={{
-          touchAction: 'none',
-          display: 'inline-block',
-          width: '100%',
-          height: '100%',
-          ...style,
-        }}
+      <SlopMap
+        mapSrc={mapSrc}
+        containerRef={containerRef}
+        slops={slops}
+        selectedSlop={selectedSlop}
       >
-        <Image src={mapSrc} alt="이미지" width={420} height={750} />
         {slops.map((slop) => {
           return (
             <React.Fragment key={slop.id}>
-              <div className={cn('absolute top-0 w-full')}>
-                <slop.Element
-                  color={
-                    selectedSlop !== slop.id && selectedSlop !== null ? 'fill-gray-40' : undefined
-                  }
-                />
-              </div>
               {slop.webcam && (
-                <div
-                  className={cn(
-                    'absolute z-10',
-                    slop.webcam.position.top,
-                    slop.webcam.position.left
-                  )}
-                >
-                  <Tooltip trigger={<CameraButton />} isOpen={selectedSlop === slop.id}>
-                    <p className={cn('body3-medium')}>{slop.webcam.name}</p>
-                  </Tooltip>
-                </div>
+                <SlopTooltip
+                  id={slop.webcam.id}
+                  name={slop.webcam.name}
+                  position={slop.webcam.position}
+                  isOpen={selectedSlop === slop.id}
+                />
               )}
             </React.Fragment>
           );
         })}
-      </animated.div>
+      </SlopMap>
     </section>
   );
 };
