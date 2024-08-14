cbbba3981558d1099aae355a8d8570f171170594
Najeong-Kim <atpeoe1023@gmail.com>
2024-08-14 22:33:10 +0900

    feat: Add WeatherList component in Discovery page

---
+++ src/app/(web)/page.tsx
@@ -0,0 +1,1 @@
+export { default } from '@/pages/discovery/ui/discovery-page';
---
+++ src/pages/discovery/ui/discovery-page.tsx
@@ -0,0 +1,16 @@
+import Header from '@/widgets/header/ui/header';
+import WeatherList from '@/features/discovery/ui/weather-list';
+import { cn } from '@/shared/lib';
+
+const Page = () => {
+  return (
+    <div className={cn('relative size-full bg-opacity-65 bg-[url("/background.png")]')}>
+      <div className={cn('size-full bg-gradient-to-b from-[#8DA3DD/20] to-transparent')}>
+        <Header />
+        <WeatherList />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
