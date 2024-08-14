87604cb723e7d9d0ad4670105004fc13f6a9241d
Najeong-Kim <atpeoe1023@gmail.com>
2024-08-14 22:53:21 +0900

    feat: Remove unused NavBar and StatusBar components

--- src/widgets/header/ui/index.ts
+++ src/widgets/header/ui/index.ts
@@ -1,4 +1,1 @@
 export { default as Header } from '@/widgets/header/ui/header';
-
-export { default as NavBar } from '@/widgets/header/ui/nav-bar';
-export { default as StatusBar } from '@/widgets/header/ui/status-bar';
