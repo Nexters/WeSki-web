f250193d2068c757eaa17036010b8e07154fe598
Najeong-Kim <atpeoe1023@gmail.com>
2024-08-14 22:14:59 +0900

    feat: Add Header component to widgets folder

--- src/widgets/header/ui/index.ts
+++ src/widgets/header/ui/index.ts
@@ -1,2 +1,4 @@
+export { default as Header } from '@/widgets/header/ui/header';
+
 export { default as NavBar } from '@/widgets/header/ui/nav-bar';
 export { default as StatusBar } from '@/widgets/header/ui/status-bar';
