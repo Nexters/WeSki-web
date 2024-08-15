fd46503ca5b5edfe26bac91aeae5c23b460718bc
Najeong-Kim <atpeoe1023@gmail.com>
2024-08-15 16:04:24 +0900

    feat: Add webcam components in detail page

--- src/pages/discovery-detail/ui/discovery-detail-page.tsx
+++ src/pages/discovery-detail/ui/discovery-detail-page.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import { useState } from 'react';
 import DiscoveryDetailSummary from '@/widgets/discovery/ui/discovery-summary';
 import { Header } from '@/widgets/header/ui';
+import { WebcamMap, WebcamSlopList } from '@/widgets/webcam/ui';
 import { DiscoveryData } from '@/entities/discovery';
+import { JISAN } from '@/entities/slop/model';
 import { cn } from '@/shared/lib';
 
 const DiscoveryDetailPage = ({ params }: { params: { resortId: number } }) => {
+  const DUMMY2 = JISAN;
+  const [selectedSlop, setSelectedSlop] = useState<string | null>(null);
+
   const discovery = DiscoveryData.find((discovery) => discovery.id === +params.resortId);
   if (!discovery) return null;
 
@@ -13,6 +19,16 @@ const DiscoveryDetailPage = ({ params }: { params: { resortId: number } }) => {
     <div className={cn('size-full')}>
       <Header />
       <DiscoveryDetailSummary {...discovery} />
+      <WebcamMap slops={DUMMY2.slops} mapSrc={DUMMY2.map} selectedSlop={selectedSlop} />
+      <WebcamSlopList
+        className={cn('bg-white')}
+        list={DUMMY2.slops.map((item) => ({
+          ...item,
+          isWebcam: !!item.webcam,
+        }))}
+        selectedSlop={selectedSlop}
+        setSelectedSlop={setSelectedSlop}
+      />
     </div>
   );
 };
