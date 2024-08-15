e2b0a5711052e2d3930e31d73c992d2ba51c27b6
Najeong-Kim <atpeoe1023@gmail.com>
2024-08-15 15:11:09 +0900

    feat: Update discovery layout

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import { cn } from '@/shared/lib';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={cn('flex size-full justify-center bg-main-5 antialiased')}>
-      <div className={cn('size-full max-w-[670px]')}>{children}</div>
+      <div className={cn('size-full max-w-[670px] bg-opacity-65 bg-[url("/background.png")]')}>
+        {children}
+      </div>
     </div>
   );
 }
--- src/pages/discovery-detail/ui/discovery-detail-page.tsx
+++ src/pages/discovery-detail/ui/discovery-detail-page.tsx
@@ -1,15 +1,15 @@
 'use client';
 
+import { Header } from '@/widgets/header/ui';
+import { cn } from '@/shared/lib';
+
 const DiscoveryDetailPage = ({ params }: { params: { resortId: number } }) => {
-  if (typeof window === 'undefined') {
-    return null;
-  }
-
-  return <DiscoveryDetailPageContent resortId={params.resortId} />;
-};
-
-const DiscoveryDetailPageContent = ({ resortId }: { resortId: number }) => {
-  return <div>{resortId}</div>;
+  return (
+    <div className={cn('size-full')}>
+      <Header />
+      <p>{params.resortId}</p>
+    </div>
+  );
 };
 
 export default DiscoveryDetailPage;
