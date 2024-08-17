9a9e136099d993cc4c8eebd8436ae7817bc17d82
Najeong-Kim <atpeoe1023@gmail.com>
2024-08-18 01:56:57 +0900

    feat: Add snow image in app download dialog

--- src/widgets/discovery-detail/ui/app-download-dialog.tsx
+++ src/widgets/discovery-detail/ui/app-download-dialog.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import android from '@public/android.png';
 import ios from '@public/ios.png';
 import logo from '@public/logo.svg';
+import snow from '@public/snow.png';
 import { CloseIcon } from '@/shared/icons';
 import { cn } from '@/shared/lib';
 
@@ -14,10 +15,17 @@ const AppDownloadDialog = ({ className, onClose }: AppDownloadDialogProps) => {
   return (
     <div
       className={cn(
-        'relative w-[341px] rounded-[7px] bg-main-5 px-[30px] pb-[26px] pt-[27px]',
+        'relative w-[341px] overflow-hidden rounded-[7px] bg-main-5 px-[30px] pb-[26px] pt-[27px]',
         className
       )}
     >
+      <Image
+        src={snow}
+        alt="snow"
+        width={191}
+        height={196}
+        className={cn('absolute right-[-21px] top-0')}
+      />
       <CloseIcon className={cn('absolute right-6 top-6 size-6 cursor-pointer')} onClick={onClose} />
       <Image src={logo} alt="logo" />
       <p className={cn('title1 mb-[39px] mt-[35px] whitespace-pre-wrap font-bold')}>
