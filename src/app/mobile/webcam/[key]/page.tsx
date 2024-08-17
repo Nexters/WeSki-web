1513b7ba59354d766f9f8e1e8f7d248092d42f35
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-18 06:38:01 +0900

    feat: 하이원 리조트 데이터 주입

--- src/app/mobile/webcam/[key]/page.tsx
+++ src/app/mobile/webcam/[key]/page.tsx
@@ -2,9 +2,11 @@ import { redirect } from 'next/navigation';
 import React from 'react';
 import WebcamMobileMapPage from '@/pages/webcam/ui/webcam-mobile-map-page';
 import { JISAN } from '@/entities/slop/model';
+import HIGH1 from '@/entities/slop/model/high1';
 
 const domainMap = {
   jisan: JISAN,
+  high1: HIGH1,
 };
 
 const Page = ({ params }: { params: { key: string } }) => {
---
+++ src/entities/slop/image/high1/hera1-slop-path.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { cn } from '@/shared/lib';
+
+const Hera1SlopPath = ({ color }: { color?: string }) => {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 376 210"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className={cn('fill-current', color ? color : 'fill-[#447EFF]')}
+        d="M172.51 22.9414C168.181 23.506 158.394 26.555 153.877 34.2339C148.231 43.8324 141.455 49.4787 143.149 53.9956"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeDasharray="3.39 3.39"
+      />
+    </svg>
+  );
+};
+
+export default Hera1SlopPath;
