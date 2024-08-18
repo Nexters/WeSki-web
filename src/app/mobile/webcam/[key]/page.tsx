36d4dfd68bd82bc9f403d2baa9236c934ea074bd
YoonHaeMin <yunhatmi@gachon.ac.kr>
2024-08-19 00:25:38 +0900

    style: change import sort

--- src/app/mobile/webcam/[key]/page.tsx
+++ src/app/mobile/webcam/[key]/page.tsx
@@ -1,12 +1,11 @@
 import { redirect } from 'next/navigation';
 import React from 'react';
 import WebcamMobileMapPage from '@/pages/webcam/ui/webcam-mobile-map-page';
-import { JISAN } from '@/entities/slop/model';
+import { EDEN, JISAN, YONGPYONG } from '@/entities/slop/model';
 import ELYSIAN_GANGCHON from '@/entities/slop/model/elysian-gangchon';
 import GONJIAM from '@/entities/slop/model/gonjiam';
 import HIGH1 from '@/entities/slop/model/high1';
 import VIVALDIPARK from '@/entities/slop/model/vivaldipark';
-import { EDEN, JISAN, YONGPYONG } from '@/entities/slop/model';
 
 const domainMap = {
   jisan: JISAN,
