dac67e621935a46b1d7b0c7ee7700c1c2fcf8540
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-18 23:55:23 +0900

    fix: stroke 색상 동적으로 적용

--- src/entities/slop/image/high1/hera1-slop-path.tsx
+++ src/entities/slop/image/high1/hera1-slop-path.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { cn } from '@/shared/lib';
 
 const Hera1SlopPath = ({ color }: { color?: string }) => {
+  const defaultStrokeColor = 'stroke-[#447EFF]';
+  const strokeColorClass = color ? color.replace('fill-', 'stroke-') : defaultStrokeColor;
+
   return (
     <svg
       width="100%"
@@ -11,7 +14,7 @@ const Hera1SlopPath = ({ color }: { color?: string }) => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        className={cn('fill-current', color ? color : 'fill-[#447EFF]')}
+        className={cn('stroke-current', strokeColorClass)}
         d="M172.51 22.9414C168.181 23.506 158.394 26.555 153.877 34.2339C148.231 43.8324 141.455 49.4787 143.149 53.9956"
         strokeWidth="1.5"
         strokeLinecap="round"
