43dd41ab333bf31dc48808ea40012f479b3546fa
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-18 23:53:20 +0900

    feat: 엘리시안 강촌 데이터 주입

--- src/app/mobile/webcam/[key]/page.tsx
+++ src/app/mobile/webcam/[key]/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation';
 import React from 'react';
 import WebcamMobileMapPage from '@/pages/webcam/ui/webcam-mobile-map-page';
 import { JISAN } from '@/entities/slop/model';
+import ELYSIAN_GANGCHON from '@/entities/slop/model/elysian-gangchon';
 import GONJIAM from '@/entities/slop/model/gonjiam';
 import HIGH1 from '@/entities/slop/model/high1';
 import VIVALDIPARK from '@/entities/slop/model/vivaldipark';
@@ -11,6 +12,7 @@ const domainMap = {
   high1: HIGH1,
   gonjiam: GONJIAM,
   vivaldipark: VIVALDIPARK,
+  'elysian-gangchon': ELYSIAN_GANGCHON,
 };
 
 const Page = ({ params }: { params: { key: string } }) => {
