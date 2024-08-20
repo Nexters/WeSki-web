f0a78668bb7945646e4657a0edc446c2f08b9a0e
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-20 20:15:44 +0900

    fix: hover와 색상문제 충돌 해결

--- src/widgets/webcam/ui/webcam-slop-list.tsx
+++ src/widgets/webcam/ui/webcam-slop-list.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import useSlopStore from '@/features/slop/hooks/useSlopStore';
 import type { Slop, Webcam } from '@/entities/slop/model/model';
@@ -17,38 +19,40 @@ const WebcamSlopList = ({ className, list, webcams, onItemClick }: WebcamSlopLis
 
   return (
     <ul className={cn('w-full xs:grid xs:grid-cols-2', className)}>
-      {list.map((item) => (
-        <li
-          key={item.id}
-          className={cn(
-            'flex h-[66px] items-center justify-between bg-gray-10 px-6',
-            'border-b border-gray-30 xs:odd:border-r',
-            item.isOpen ? 'cursor-pointer hover:bg-main-5' : 'cursor-not-allowed',
-            selectedSlop === item.id && 'bg-main-5'
-          )}
-          onClick={() => {
-            if (selectedSlop === item.id) {
-              setSelectedSlop(null);
-            } else {
-              setSelectedSlop(item.id);
-            }
-            if (item.webcamId) {
-              const webcam = webcams.find((webcam) => webcam.id === item.webcamId);
-              if (!webcam) return;
-              onItemClick({
-                scale: webcam.scale,
-                id: webcam.id,
-              });
-            }
-          }}
-        >
-          <div className={cn('flex items-center gap-2')}>
-            <p className={cn('title3-semibold', !item.isOpen && 'opacity-30')}>{item.name}</p>
-            {item.webcamId && <CameraButton />}
-          </div>
-          <LevelChip className={cn(!item.isOpen && 'opacity-30')} level={item.level} />
-        </li>
-      ))}
+      {list.map((item) => {
+        return (
+          <li
+            key={item.id}
+            className={cn(
+              'flex h-[66px] items-center justify-between bg-gray-10 px-6',
+              'border-b border-gray-30 xs:odd:border-r',
+              item.isOpen ? 'cursor-pointer hover:bg-main-5' : 'cursor-not-allowed',
+              selectedSlop === item.id ? 'bg-main-5' : 'bg-inherit hover:bg-inherit'
+            )}
+            onClick={() => {
+              if (selectedSlop === item.id) {
+                setSelectedSlop(null);
+              } else {
+                setSelectedSlop(item.id);
+              }
+              if (item.webcamId) {
+                const webcam = webcams.find((webcam) => webcam.id === item.webcamId);
+                if (!webcam) return;
+                onItemClick({
+                  scale: webcam.scale,
+                  id: webcam.id,
+                });
+              }
+            }}
+          >
+            <div className={cn('flex items-center gap-2')}>
+              <p className={cn('title3-semibold', !item.isOpen && 'opacity-30')}>{item.name}</p>
+              {item.webcamId && <CameraButton />}
+            </div>
+            <LevelChip className={cn(!item.isOpen && 'opacity-30')} level={item.level} />
+          </li>
+        );
+      })}
     </ul>
   );
 };
