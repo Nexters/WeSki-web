682641009fef13dd23ea4d66db907cd0471944a5
Najeong-Kim <atpeoe1023@gmail.com>
2024-08-22 01:44:05 +0900

    feat: Add side background image

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,17 +1,32 @@
 'use client';
 
+import Image from 'next/image';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
+import left from '@public/downloads/left.png';
+import right from '@public/downloads/right.png';
 import { cn } from '@/shared/lib';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <GoogleAnalytics trackPageViews />
-      <div className={cn('flex size-full justify-center bg-main-5 antialiased')}>
+      <div
+        className={cn(
+          'flex size-full min-w-[360px] items-center justify-evenly bg-main-5 antialiased'
+        )}
+      >
+        <div className={cn('hidden lg:block')}>
+          <Image src={left} alt="left" width={270} height={320} />
+        </div>
         <div
-          className={cn('size-full max-w-[670px] bg-opacity-65 bg-[url("/assets/background.png")]')}
+          className={cn(
+            'h-screen w-full max-w-[670px] overflow-scroll bg-opacity-65 bg-[url("/assets/background.png")]'
+          )}
         >
           {children}
+        </div>
+        <div className={cn('hidden lg:block')}>
+          <Image src={right} alt="right" width={270} height={320} />
         </div>
       </div>
     </>
