f176f53b6373036afafa490936ac15ce3e968efb
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-21 22:46:58 +0900

    feat: GA추가

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,15 +1,19 @@
 'use client';
 
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 import { cn } from '@/shared/lib';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={cn('flex size-full justify-center bg-main-5 antialiased')}>
-      <div
-        className={cn('size-full max-w-[670px] bg-opacity-65 bg-[url("/assets/background.png")]')}
-      >
-        {children}
+    <>
+      <GoogleAnalytics trackPageViews />
+      <div className={cn('flex size-full justify-center bg-main-5 antialiased')}>
+        <div
+          className={cn('size-full max-w-[670px] bg-opacity-65 bg-[url("/assets/background.png")]')}
+        >
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
