286cbdd3bf29d58396559a61906f0448360b190f
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-08-22 18:29:29 +0900

    fix: tooltip에 arrow icon 추가

---
+++ src/shared/icons/arrow-right.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ArrowRightIcon = () => {
+  return (
+    <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M6.5 4.5L10.5 8.5L6.5 12.5"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowRightIcon;
