9d485610f6a7265db7f466bccb886066efc3b8da
Najeong-Kim <atpeoe1023@gmail.com>
2024-08-22 10:17:03 +0900

    feat: Add og image and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,17 @@ import { Toaster } from '@/shared/ui/toaster';
 import Providers from './_providers';
 
 export const metadata: Metadata = {
-  title: 'WeSki',
-  description: 'We Ski',
+  title: 'WeSki : 스키장 혼잡도 확인 서비스',
+  description:
+    '위스키는 웹캠, 날씨, 슬로프 등 실시간 스키장 정보를 탐색할 수 있는 스키장 큐레이션 서비스입니다.',
+  openGraph: {
+    images: [
+      {
+        url: '/assets/og.png',
+        alt: 'WeSki',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
