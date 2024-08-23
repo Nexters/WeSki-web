edd42d331b141bcf9163915ec20dafacb1f9e7c9
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2024-08-23 23:27:26 +0900

    fix: Add app download link

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
+import { useCallback } from 'react';
+import { toast } from 'sonner';
 import left from '@public/downloads/left.png';
 import android from '@public/downloads/right-android.png';
 import ios from '@public/downloads/right-ios.png';
@@ -10,6 +13,10 @@ import { cn } from '@/shared/lib';
 import { SnowConfetti } from '@/shared/ui/snow-confetti';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const handleAndroid = useCallback(() => {
+    toast.error('준비중입니다.');
+  }, []);
+
   return (
     <>
       <GoogleAnalytics trackPageViews />
@@ -36,8 +43,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         >
           <Image src={text} alt="text" width={194} />
           <div className={cn('flex w-[200px] justify-between')}>
-            <Image src={ios} alt="ios" width={95} />
+            <Link
+              href="https://apps.apple.com/kr/app/weski-%EC%8A%A4%ED%82%A4%EC%9E%A5-%ED%81%90%EB%A0%88%EC%9D%B4%EC%85%98-%ED%94%8C%EB%9E%AB%ED%8F%BC/id6642660900"
+              target="_blank"
+            >
+              <Image src={ios} alt="ios" width={95} />
+            </Link>
+            <button onClick={handleAndroid}>
             <Image src={android} alt="android" width={95} />
+            </button>
           </div>
         </div>
       </div>
--- src/features/discovery-detail/ui/app-download-dialog.tsx
+++ src/features/discovery-detail/ui/app-download-dialog.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { useCallback } from 'react';
+import { toast } from 'sonner';
 import logo from '@public/assets/logo.svg';
 import snow from '@public/assets/snow.png';
 import android from '@public/downloads/android.png';
@@ -12,6 +15,10 @@ interface AppDownloadDialogProps {
 }
 
 const AppDownloadDialog = ({ className, onClose }: AppDownloadDialogProps) => {
+  const handleAndroid = useCallback(() => {
+    toast.error('준비중입니다.');
+  }, []);
+  
   return (
     <div
       className={cn(
@@ -30,10 +37,13 @@ const AppDownloadDialog = ({ className, onClose }: AppDownloadDialogProps) => {
         {'지금 앱 다운로드하고\n블라인드된 정보를\n매일 확인해보세요'}
       </p>
       <div className={cn('flex gap-3')}>
-        <button>
+        <Link
+          href="https://apps.apple.com/kr/app/weski-%EC%8A%A4%ED%82%A4%EC%9E%A5-%ED%81%90%EB%A0%88%EC%9D%B4%EC%85%98-%ED%94%8C%EB%9E%AB%ED%8F%BC/id6642660900"
+          target="_blank"
+        >
           <Image src={ios} alt="ios" />
-        </button>
-        <button>
+        </Link>
+        <button onClick={handleAndroid}>
           <Image src={android} alt="android" />
         </button>
       </div>
