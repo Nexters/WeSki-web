784c18c2cab99ff29980e2b8c769cde15c8a309b
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-11-07 23:10:42 +0900

    fix: qa 241107 (#46)
    * fix: update og image
    
    * fix: update favicon
    
    * fix: update card opacity

--- src/shared/ui/card.tsx
+++ src/shared/ui/card.tsx
@@ -7,7 +7,7 @@ interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
 const Card = ({ className, children, ...props }: CardProps) => (
   <div
     className={cn(
-      'backdrop-blur-px rounded-[15px] border border-white/30 bg-gradient-to-br from-white/90 to-white/30 shadow-[2px_10px_29px_0px_rgba(47,49,65,0.02)]',
+      'backdrop-blur-px rounded-[15px] border border-white/30 bg-gradient-to-br from-white/90 to-white/50 shadow-[2px_10px_29px_0px_rgba(47,49,65,0.02)]',
       className
     )}
     {...props}
