6058d9f3bb8b89db104a30b8f6f69af158ed97f6
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2024-11-09 09:31:14 +0900

    refactor: delete getDiscoveries

--- src/entities/discovery/api/get-discoveries.ts
+++
@@ -1,9 +0,0 @@
-import { DiscoveryData } from '../model';
-import type { Discovery } from '../model';
-
-export const getDiscoveries = async (): Promise<Discovery[]> => {
-  // const result = await apiClient.get<Discovery[]>('/discoveries');
-  const result = DiscoveryData;
-
-  return result;
-};
--- src/entities/discovery/api/query/discovery.queries.ts
+++ src/entities/discovery/api/query/discovery.queries.ts
@@ -1,16 +1,9 @@
 import { queryOptions } from '@tanstack/react-query';
-import { getDiscoveries } from '../get-discoveries';
 import { getVote } from '../get-vote';
 
 export const discoveryQueries = {
   all: () => ['discovery'],
 
-  listQueryKey: () => [...discoveryQueries.all(), 'list'],
-  list: () =>
-    queryOptions({
-      queryKey: [...discoveryQueries.listQueryKey()],
-      queryFn: () => getDiscoveries(),
-    }),
   voteQueryKey: (resortId: number) => [...discoveryQueries.all(), 'vote', resortId],
   vote: (resortId: number) =>
     queryOptions({
