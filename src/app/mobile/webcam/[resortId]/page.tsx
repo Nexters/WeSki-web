6feffdc3a3ffe50a8bbf794faffa8c38f402004e
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2024-11-12 08:00:56 +0900

    hotfix: fix webcam page error

--- src/app/mobile/webcam/[resortId]/page.tsx
+++ src/app/mobile/webcam/[resortId]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import WebcamMobileMapPage from '@/views/webcam/ui/webcam-mobile-map-page';
 
 const Page = ({ params }: { params: { resortId: string } }) => {
-  <WebcamMobileMapPage resortId={+params.resortId} />;
+  return <WebcamMobileMapPage resortId={+params.resortId} />;
 };
 
 export default Page;
