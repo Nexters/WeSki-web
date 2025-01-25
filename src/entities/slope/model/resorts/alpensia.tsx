1ac0eb8c3ba121baff24948f40efd7af3b3118e7
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2025-01-25 13:39:44 +0900

    feat: add alpensia resort data

---
+++ src/entities/slope/model/resorts/alpensia.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+'use client';
+
+import MapImage from '@public/maps/alpensia.png';
+import * as Slope from '../image/alpensia';
+import type { ResortConstant } from '../model';
+
+export const ALPENSIA: ResortConstant = {
+  MapComponent: MapImage,
+  slopes: [
+    {
+      id: 0,
+      PathAsset: Slope.AlphaPath,
+    },
+    {
+      id: 0,
+      PathAsset: Slope.BravoPath,
+    },
+    {
+      id: 0,
+      PathAsset: Slope.CharliePath,
+    },
+    {
+      id: 0,
+      PathAsset: Slope.DeltaPath,
+    },
+    {
+      id: 0,
+      PathAsset: Slope.EchoPath,
+    },
+    {
+      id: 0,
+      PathAsset: Slope.FoxtrotPath,
+    },
+  ],
+  webcams: [
+    {
+      id: 1,
+      key: '',
+      position: [0, 0],
+      scale: 1,
+    },
+    {
+      id: 2,
+      key: '',
+      position: [0, 0],
+      scale: 1,
+    },
+    {
+      id: 3,
+      key: '',
+      position: [0, 0],
+      scale: 1,
+    },
+    {
+      id: 4,
+      key: '',
+      position: [0, 0],
+      scale: 1,
+    },
+  ],
+};
