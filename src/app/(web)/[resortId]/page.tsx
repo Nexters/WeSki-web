23bdee95c12db4a6791a3b25d358135f234a095a
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-12-09 20:53:49 +0900

    feat: update resort metadata and improve SEO (#62)

--- src/app/(web)/[resortId]/page.tsx
+++ src/app/(web)/[resortId]/page.tsx
@@ -12,10 +12,10 @@ export async function generateMetadata({
   const resort = ResortData.find((resort) => resort.id === +resortId);
 
   return {
-    title: resort ? `${resort.name} - WeSki` : 'WeSki : 스키장 혼잡도 확인 서비스',
-    description: resort
-      ? `${resort.name}의 실시간 정보를 확인해보세요.`
-      : '위스키는 웹캠, 날씨, 슬로프 등 실시간 스키장 정보를 탐색할 수 있는 스키장 큐레이션 서비스입니다.',
+    title: resort?.metadata?.title ?? '전국 스키장 실시간 웹캠 & 날씨 정보 | WeSki',
+    description:
+      resort?.metadata?.description ??
+      '전국 스키장의 실시간 웹캠, 눈 상태, 설질, 혼잡도, 날씨 정보를 한눈에! X5스키장과 시즌권이 있는 모든 리조트의 스키장 정보를 빠르게 확인하세요.',
     alternates: {
       canonical: `/${resortId}`,
     },
